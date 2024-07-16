06d8f101885f7a157fab5de0634810e3e3fb2eec
ashish-vs <ashish.negi@vectoscalar.com>
2024-07-16 15:06:36 +0530

    Minor

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,10 +2,13 @@ import { UserModel } from "./user.model";
 import { ProjectModel } from "./project.model";
 import { OrganizationModel } from "./organization.model";
 import { MicroserviceModel } from "./microservice.model";
-
+import { APILogModel } from './apiLogs.model';
+import { EndpointModel } from './endpoint.model';
 export {
   UserModel,
   ProjectModel,
   OrganizationModel,
-  MicroserviceModel
+  MicroserviceModel,
+  APILogModel,
+  EndpointModel,
 };
